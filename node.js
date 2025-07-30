c7028cbce8248a16e8c2fdb0d90644f64372d95e
vasavigarapati1998-lab <vasavi.garapati1998@gmail.com>
2025-07-30 20:43:58 +0530

    Update node.js

--- node.js
+++ node.js
@@ -9,6 +9,7 @@ const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello, World!\n');
+   res.end('Hello, World!\n');
 });
 
 server.listen(3000, () => {
